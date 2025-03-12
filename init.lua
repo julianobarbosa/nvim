ac897de17b273ca59a3d74dbba543d50d02306ce
juliano.barbosa <julianomb@gmail.com>
2025-03-12 18:13:47 -0300

    feat: add keybindings for buffer cycling in init.lua Signed-off-by: juliano.barbosa <julianomb@gmail.com>

--- init.lua
+++ init.lua
@@ -174,6 +174,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- TIP: to cycle buffers
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", { noremap = true, silent = true })
+
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
